d775219bf71e5a9ea4bdab9e6880af5efb05e343
weshy007 <josephwaweru96@gmail.com>
2021-06-08 19:32:56 +0300

    skills styling and functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// MENU SHOW &&& HIDDEN MENU
+// ================= MENU SHOW &&& HIDDEN MENU ===================
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
@@ -29,3 +29,23 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// ===============ACCORDION SKILLS==============
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
